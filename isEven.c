7b652e6791ec413501a4ded62b0d941056d390f8
LiamG. <l530g592@ku.edu>
2026-02-05 11:09:24 -0600

    added isOdd function, added comments explaining function processes

--- isEven.c
+++ isEven.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "isEven.h"
-int isEven(int num)
+int isEven(int num) // prints message to console if num is even
 {
-	if (num % 2 == 0){
+	if (num % 2 == 0){ 
 		 printf("%d is even.\n", num);
 	}
 	return 0;
 }
-int isOdd(int num)
+int isOdd(int num) // prints message to console if num is odd
 {
 	if (num % 2 != 0){
 		 printf("%d is odd.\n", num);
