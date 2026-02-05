ba4cea983220d5867153bd1666412fdaa1e344f5
LiamG. <l530g592@ku.edu>
2026-02-05 11:07:51 -0600

    Added comments to main.c explaining function processes

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 int main() {
     int num;
     printf("Enter an integer: ");
-    scanf("%d", &num);
-    isEven(num);
+    scanf("%d", &num); // takes int input from console, stores in var num
+    isEven(num); //calls function isEven on var num, prints a message to console if the value is even
     // add isOdd(num);
-    isOdd(num);
+    isOdd(num); //calls function isOdd on var num, prints a message to console if the value is odd
     return 0;
 }
